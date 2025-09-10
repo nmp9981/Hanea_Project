de27a7e6e6be635500b1d4cf501d30e55800144d
tybnasgo <tybnasgo@naver.com>
2025-09-11 04:02:01 +0900

    타일 속성 추가

--- Assets/Scripts/InGame/Map/Tile.cs
+++ Assets/Scripts/InGame/Map/Tile.cs
@@ -15,11 +15,25 @@ public enum Planet
     Count
 }
 
+/// <summary>
+/// 타일 위치
+/// </summary>
+public struct TilePosition
+{
+    public int areaNum;//구역 번호
+    public float x, y;//좌표
+};
+
+
 public class Tile : MonoBehaviour
 {
     [SerializeField]
-    private Planet _planetType;
+    private Planet _planetType;//행성 타입
+    private TilePosition _tilePos;//타일 위치
+    private int _tilePower = 0;//타일 파워
 
     //읽기 전용
     public Planet PlanetType => _planetType;
+    public TilePosition TilePos => _tilePos;
+    public int TilePower => _tilePower;
 }
---
+++ Assets/Scripts/InGame/Map/TileSystem.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public static class TileSystem
+{
+    /// <summary>
+    /// 두 타일간 거리
+    /// </summary>
+    /// <returns></returns>
+    public static int DistTileToTile(TilePosition tileA, TilePosition tileB)
+    {
+        int dist = 0;
+
+
+        return dist;
+    }
+}
