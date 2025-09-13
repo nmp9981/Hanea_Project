616f8ff7756f35766b3d669511456afe3a4331f5
tybnasgo <tybnasgo@naver.com>
2025-09-13 11:34:53 +0900

    두 타일간 거리, 파워 총합 구하는 로직 추가

--- Assets/Scripts/InGame/Map/Tile.cs
+++ Assets/Scripts/InGame/Map/Tile.cs
@@ -12,6 +12,7 @@ public enum Planet
     Gaia,
     Dimension,
     Black,
+    None,
     Count
 }
 
@@ -21,7 +22,7 @@ public enum Planet
 public struct TilePosition
 {
     public int areaNum;//구역 번호
-    public float x, y;//좌표
+    public float x, y, z;//큐브 좌표계
 };
 
 
@@ -31,9 +32,26 @@ public class Tile : MonoBehaviour
     private Planet _planetType;//행성 타입
     private TilePosition _tilePos;//타일 위치
     private int _tilePower = 0;//타일 파워
+    private bool _isUnion;//연방 여부
 
     //읽기 전용
     public Planet PlanetType => _planetType;
     public TilePosition TilePos => _tilePos;
     public int TilePower => _tilePower;
+    public bool isUnion => _isUnion;
+
+    private void Awake()
+    {
+        Change_CubeCoordinateSystem();
+    }
+
+    /// <summary>
+    /// 큐브좌표계로 변환
+    /// </summary>
+    void Change_CubeCoordinateSystem()
+    {
+        _tilePos.x = transform.position.x;
+        _tilePos.z = transform.position.y;
+        _tilePos.y = -TilePos.x-TilePos.z;
+    }
 }
--- Assets/Scripts/InGame/Map/TileSystem.cs
+++ Assets/Scripts/InGame/Map/TileSystem.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public static class TileSystem
@@ -8,9 +9,25 @@ public static class TileSystem
     /// <returns></returns>
     public static int DistTileToTile(TilePosition tileA, TilePosition tileB)
     {
-        int dist = 0;
-
-
-        return dist;
+        float diffX = Mathf.Abs(tileA.x - tileB.x);
+        float diffY = Mathf.Abs(tileA.y - tileB.y);
+        float diffZ = Mathf.Abs(tileA.z - tileB.z);
+        float dist = (diffX+diffY+diffZ)/2;
+        return (int)Mathf.Round(dist);
     }
+
+    /// <summary>
+    /// 파워값 총합 구하기
+    /// </summary>
+    /// <returns></returns>
+    public static int SumPower(List<Tile> tileList)
+    {
+        int sum = 0;
+        foreach (Tile tile in tileList)
+        {
+            sum += tile.TilePower;
+        }
+        return sum;
+    }
+
 }
