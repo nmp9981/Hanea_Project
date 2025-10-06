7a49c1d1bbe96ff8b3f6485460ee6e6493494162
tybnasgo <tybnasgo@naver.com>
2025-10-06 17:00:18 +0900

    플레이어 건물영역 UI 제작, 바인딩

--- Assets/Scripts/InGame/Manager/BuildingManager.cs
+++ Assets/Scripts/InGame/Manager/BuildingManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildingManager : MonoBehaviour
 {
@@ -13,6 +14,24 @@ public class BuildingManager : MonoBehaviour
     [SerializeField]
     public List<BuildingData> buildingDataList = new();
 
+    //건물 UI
+    [SerializeField]
+    private List<Image> mineImage_UIList = new();
+    public Stack<Image> mineImage_UIStack = new();
+    [SerializeField]
+    private List<Image> tradingStation_UIList = new();
+    public Stack<Image> tradingStation_UIStack = new();
+    [SerializeField]
+    private List<Image> researchLab_UIList = new();
+    public Stack<Image> researchLab_UIStack = new();
+    [SerializeField]
+    private List<Image> academy_UIList = new();
+    public Stack<Image> academy_UIStack = new();
+    [SerializeField]
+    private List<Image> institute_UIList = new();
+    public Stack<Image> institute_UIStack = new();
+
+
     private void Awake()
     {
         if (Instance == null)
@@ -24,8 +43,36 @@ public class BuildingManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        EnrollBuldingImage();//건물 이미지 등록
     }
    
+    /// <summary>
+    /// 건물 이미지 등록
+    /// </summary>
+    private void EnrollBuldingImage()
+    {
+        foreach(var image in mineImage_UIList)
+        {
+            mineImage_UIStack.Push(image);
+        }
+        foreach (var image in tradingStation_UIList)
+        {
+            tradingStation_UIStack.Push(image);
+        }
+        foreach (var image in researchLab_UIList)
+        {
+            researchLab_UIStack.Push(image);
+        }
+        foreach (var image in academy_UIList)
+        {
+            academy_UIStack.Push(image);
+        }
+        foreach (var image in institute_UIList)
+        {
+            institute_UIStack.Push(image);
+        }
+    }
     /// <summary>
     /// 건물 짓기
     /// 어느 타일에 해당 건물을 짓는다.
